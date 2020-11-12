f1dc518ecca35596cef78fbb2c45d984445009ef
Eddie Tian <eddie_tian@outlook.com>
2020-11-12 07:18:59 -0700

    cleaning up

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import serial
 
 degree_sign= u'\N{DEGREE SIGN}'
 
-#temp=graph.Graph("Temperature over the past 12 hours", "Time before current time, hours", "Temperature, "+degree_sign+"C", "temp.png")
-#ph=graph.Graph("ph over the past 12 hours", "Time before current time, hours", "ph", "ph.png")
-#humidity=graph.Graph("Humidity over the past 12 hours", "Time before current time, hours", "Humidity, %RH", "humidity.png")
+temp=graph.Graph("Temperature over the past 12 hours", "Time before current time, hours", "Temperature, "+degree_sign+"C", "temp.png")
+ph=graph.Graph("ph over the past 12 hours", "Time before current time, hours", "ph", "ph.png")
+humidity=graph.Graph("Humidity over the past 12 hours", "Time before current time, hours", "Humidity, %RH", "humidity.png")
 
 ser = serial.Serial('COM3', 9600, timeout=1)
 
@@ -22,8 +22,6 @@ while(True):
         sleep(1)
         #this is to prevent modulous from triggering multiple times in a second
         sensors.update()
-        print(sensors.get_temp())
-        print(sensors.get_humidity())
     # if int(ttime)%(config.ON_TIME)==0:
     #     sprayers.spray(config.OFF_TIME)
 
